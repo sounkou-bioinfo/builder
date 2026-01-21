3d083d3d597d94bf7b47e15d2e6b00be2a654e4a
John Coene <jcoenep@gmail.com>
2026-01-21 23:58:38 +0100

    fix: help

--- src/main.c
+++ src/main.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     printf("  -input <path>         Input directory (default: srcr/)\n");
     printf("  -output <path>        Output directory (default: R/)\n");
     printf("  -noclean              Skip cleaning output directory before processing\n");
-    printf("  -D<NAME>              Define directives, e.g.: -DDEBUG -DVALUE 42\n");
+    printf("  -D<NAME> <value>      Define directives, e.g.: -DDEBUG -DVALUE 42\n");
     printf("  -plugin               Use plugins, e.g.: -plugin pkg::plugin pkg::plugin2\n");
     printf("  -import               Import .rh files, e.g.: -import inst/main.rh pkg::main.rh\n");
     printf("  -prepend              Path to file to prepend to every output file (e.g.: license)\n");
